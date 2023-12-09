fd2ec89e4416fb648f7058d352f0048fce66f2b7
ryan echternacht <ryanechternacht@ryans-MacBook-Pro-2.local>
2023-12-09 10:42:53 -0500

    day 8 part 2 (damn cycles!)

--- src/day08.clj
+++ src/day08.clj
@@ -3,6 +3,7 @@
 
 (def sample-file "resources/day08-sample.txt")
 (def sample-file-2 "resources/day08-sample-2.txt")
+(def sample-file-3 "resources/day08-sample-3.txt")
 (def input-file "resources/day08.txt")
 
 (defn read-data [file]
@@ -19,6 +20,7 @@
     {:rls rls
      :nodes nodes}))
 
+;; p1
 (let [{:keys [nodes rls]} (read-data input-file)
       rls (cycle rls)]
   (loop [node "AAA"
@@ -26,3 +28,74 @@
     (if (= node "ZZZ")
       steps
       (recur (nodes [node (nth rls steps)]) (inc steps)))))
+
+(defn third [[_ _ t]]
+  t)
+
+(defn third-is-z [coll]
+  (= \Z (third coll)))
+
+(defn cycle-nth [coll index]
+  (let [i (mod index (count coll))]
+    (nth coll i)))
+
+(comment 
+  (third [1 2 3])
+  (third [1 2])
+  (third "ABCD")
+  (third-is-z "ABZ")
+  (third-is-z "ABD")
+
+  (cycle-nth "abc123" 3)
+  (cycle-nth "abc123" 6)
+  (cycle-nth "abc123" 9)
+  ;
+  )
+
+;; p2
+;; (let [{:keys [nodes rls]} (read-data input-file)
+;;       third-is-a (->> nodes
+;;                       (map first)
+;;                       (map first)
+;;                       distinct
+;;                       (filter #(= \A (third %))))]
+;;   (loop [current-nodes ["ZZZ" "NNZ" "PMZ" "DKZ" "DBZ" "NJZ"]
+;;          steps 10818234074807]
+;;     (Thread/sleep 1000)
+;;     (println steps current-nodes)
+;;     ;; (when (zero? (mod steps 100000))
+;;     ;;   (println steps current-nodes))
+;;     (if (every? third-is-z current-nodes)
+;;       steps
+;;       (let [rl (cycle-nth rls steps)]
+;;         (recur (map #(nodes [% rl]) current-nodes) (inc steps))))))
+
+(let [{:keys [nodes rls]} (read-data input-file)]
+  (loop [node "VMD"
+         steps 22200]
+    (let [rl (cycle-nth rls steps)]
+      ;; (Thread/sleep 100)
+      ;; (println rl)
+      (if (third-is-z node)
+        [steps node rl]
+        (recur (nodes [node rl]) (inc steps))))))
+
+;; AAA -> 17141 -> ZZZ -> 34282 -> ZZZ
+;; AAA 17141 offset, 17141 cycle length
+
+;; XQA -> 16579 -> NNZ -> 33158 -> NNZ
+;; 16579 offset, 16579 cycle length
+
+;; SKA -> 18827 -> PMZ -> 37654 -> PMZ
+;; 18827 offset and cycle
+
+;; NQA -> 12083 -> DKZ -> 24166 -> DKZ
+;; 12083 offset and cycle
+
+;; LJA -> 13207 -> DBZ -> 26414 -> DBZ
+;; 13207 offset and cycle
+
+;; NVA -> 22199 -> NJZ -> 44398 -> NJZ
+;; 22199 offset and cycle length
+
+;; 10818234074807
